2f16daa15224b1f4dabbd3c2fec66dd646ca476d
EC2 Default User <ec2-user@ip-172-31-13-89.us-west-1.compute.internal>
2022-08-28 04:31:39 +0000

    finished jQuery slideToggle exercise

---
+++ jQuery_slideToggle_accordian/js/keypress.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  // alert("Hello World!")
+  let $form = $("form");
+  let $currentKey;
+
+  $form.on("click", (event) => {
+    event.preventDefault();
+    $currentKey = $("#currentkey").val();
+    
+    $(document).off("keypress").on("keypress", (e) => {
+      let pressedKey = e.key;
+      if ($currentKey === pressedKey) {
+        $("a").trigger("click");
+      }
+    })
+  })
+
+  $("a").on("click", (e) => {
+    e.preventDefault();
+    $("#accordian").slideToggle();
+  })
+});
